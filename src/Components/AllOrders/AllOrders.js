b9a3e7885d3d171bf6a8fc25622ad8700eb8cbc5
Dev70 <Shanto9585>
2022-06-20 20:43:46 +0600

    update

--- src/Components/AllOrders/AllOrders.js
+++ src/Components/AllOrders/AllOrders.js
@@ -8,43 +8,49 @@ const AllOrders = () => {
 
 
     useEffect(() => {
-        fetch(`https://secure-coast-41570.herokuapp.com/allOrders`)
+        fetch(`http://localhost:5000/getEmploys`)
             .then((res) => res.json())
             .then((data) => setOrders(data));
     }, []);
 
     return (
-
         <>
             < div className="container all-order-container" >
                 <div className="text-center pb-3">
                     <h1 className="mb-5 text-center pt-5">Your Ordered <span className="text-danger">{orders.length}</span>  Products....!!!!!</h1>
                 </div>
 
+                <button data-bs-toggle="modal"
+                    data-bs-target="#staticBackdrop"
+                    className="btn btn-danger">New</button>
                 <table className="table table-dark" style={{ width: "100%" }}>
+
                     <thead  >
                         <tr className="bg-dark text-white mb-3 p-2" style={{ border: "1px solid red" }}>
 
                             <th >Number</th>
-                            <th >Product</th>
-                            <th >Image</th>
-                            <th >Price</th>
+                            <th >Title</th>
+                            {/* <th >Image</th> */}
+                            <th >First Name</th>
+                            <th >Last Name</th>
                             <th >User Email</th>
-                            <th >User Address</th>
-                            <th >Status</th>
-                            <th >Update</th>
+                            <th >User Mobile Number</th>
+                            <th >Date</th>
+
                         </tr>
                     </thead>
                     {orders?.map((order, index) => (
                         <tbody key={order._id}>
                             <tr role="row" style={{ border: "2px solid gray" }} >
                                 <th scope="row">{index + 1}</th>
-                                <td>{order.name}</td>
-                                <td><img style={{ width: "70px", height: "50px" }} src={order.img} alt="" /></td>
-                                <td>{order.price}</td>
-                                <td>{order.email}</td>
-                                <td>{order.address}</td>
-                                <td>
+                                <td>{order.Title}</td>
+                                <td>{order.FirstName}</td>
+                                {/* <td><img style={{ width: "70px", height: "50px" }} src={order.img} alt="" /></td> */}
+                                <td>{order.LastName}</td>
+                                <td>{order.Email}</td>
+                                <td>{order.MobileNumber}</td>
+                                <td>{order.date}</td>
+                                {/* <td>
                                     <div >
                                         <select className="pending p-2 ">
                                             <option defaultValue={order.status}>{order.status}</option>
@@ -53,12 +59,8 @@ const AllOrders = () => {
                                             <option defaultValue="pending">Cancelled</option>
                                         </select>
                                     </div>
-                                </td>
-                                <td>
-                                    <button data-bs-toggle="modal"
-                                        data-bs-target="#staticBackdrop"
-                                        className="btn btn-danger">update</button>
-                                </td>
+                                </td> */}
+
                             </tr>
                         </tbody>
 
--- src/Components/AllOrders/AllorderModal.js
+++ src/Components/AllOrders/AllorderModal.js
@@ -2,10 +2,24 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 
 const AllorderModal = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
     const onSubmit = data => {
-        console.log(data);
 
+        fetch(`http://localhost:5000/addEmploys`, {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(data),
+        })
+            .then((res) => res.json())
+            .then((result) => {
+                console.log(result)
+                alert(
+                    'Employ Posted Successfully.',
+                )
+                reset()
+            });
+
+        console.log(data)
     };
 
 
@@ -17,7 +31,7 @@ const AllorderModal = () => {
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="staticBackdropLabel">Login</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => reset()}></button>
                     </div>
                     <div class="modal-body">
                         <div class=" col-12 ">
@@ -25,13 +39,16 @@ const AllorderModal = () => {
 
                                 <form onSubmit={handleSubmit(onSubmit)}>
                                     <label for="exampleInputPassword1">First Name</label>
-                                    <input class="form-control" type="text" placeholder="First name" {...register("First name", { required: true, maxLength: 80 })} />
+                                    <input class="form-control" type="text" placeholder="First Name" {...register("FirstName", { required: true, maxLength: 80 })} />
+                                    {errors.FirstName && <span>This field is required</span>}
+
+
                                     <label for="exampleInputPassword1">Last Name</label>
-                                    <input class="form-control" type="text" placeholder="Last name" {...register("Last name", { required: true, maxLength: 100 })} />
+                                    <input class="form-control" type="text" placeholder="Last Name" {...register("LastName", { required: true, maxLength: 100 })} />
                                     <label for="exampleInputPassword1">Email Name</label>
                                     <input class="form-control" type="text" placeholder="Email" {...register("Email", { required: true, pattern: /^\S+@\S+$/i })} />
                                     <label for="exampleInputPassword1">Mobile Number</label>
-                                    <input class="form-control" type="tel" placeholder="Mobile number" {...register("Mobile number", { required: true, minLength: 6, maxLength: 12 })} />
+                                    <input class="form-control" type="tel" placeholder="Mobile number" {...register("MobileNumber", { required: true, minLength: 6, maxLength: 12 })} />
 
                                     <label for="exampleInputPassword1">Title</label>
                                     <select class="form-control" {...register("Title", { required: true })}>
@@ -40,6 +57,14 @@ const AllorderModal = () => {
                                         <option value="Miss">Miss</option>
                                         <option value="Dr">Dr</option>
                                     </select>
+                                    <h6>Hobby</h6>
+                                    <label for="exampleInputPassword1">Games</label>
+                                    <input  {...register("hobby", { required: true })} type="checkbox" value="Games" />
+                                    <label for="exampleInputPassword1">Sports</label>
+                                    <input  {...register("hobby", { required: true })} type="checkbox" value="sports" />
+                                    <label for="exampleInputPassword1">Movies</label>
+                                    <input  {...register("hobby", { required: true })} type="checkbox" value="movies" />
+                                    <h6>Developer</h6>
                                     <label for="exampleInputPassword1">Yes</label>
                                     <input  {...register("Developer", { required: true })} type="radio" value="Yes" />
 
@@ -47,7 +72,7 @@ const AllorderModal = () => {
                                     <input {...register("Developer", { required: true })} type="radio" value="No" />
                                     <br />
                                     <label for="exampleInputPassword1">Date</label>
-                                    <input {...register("Developer", { required: true })} type="date" />
+                                    <input {...register("date", { required: true })} type="date" />
 
                                     <input type="submit" />
                                 </form>
